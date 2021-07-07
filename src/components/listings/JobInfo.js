ea5401c29732303f55ea1685c090007462dc3dd8
Jordan Lewis <jlewis2008@live.com>
2021-07-06 20:53:22 -0400

    filters for mobile and job info listing css

--- src/components/listings/JobInfo.js
+++ src/components/listings/JobInfo.js
@@ -2,17 +2,25 @@ import React from 'react';
 
 const JobInfo = ({ listing }) => {
     return (
-        <div>
-            <img src={listing.logo} />
-            <p>{listing.company}</p>
-            {listing.new ? <p>NEW!</p> : null}
-            {listing.featured ? <p>FEATURED</p> : null}
-            <h3>{listing.position}</h3>
-            <p>{listing.postedAt}</p>
-            &#8226;
-            <p>{listing.contract}</p>
-            &#8226;
-            <p>{listing.location}</p>
+        <div className="job__info--container">
+            <img src={listing.logo} className="job__info--picture"/>
+            <div className="job__info--listing-container">
+                <p className="job__info--company">{listing.company}</p>
+                <div className="job__info--extra-container">
+                    {listing.new ? <p className="job__info--new job__info--extra">NEW!</p> : null}
+                    {listing.featured ? <p className="job__info--featured job__info--extra">FEATURED</p> : null}
+                </div>
+            </div>
+            <div className="job__info--position-container">
+                <h3 className="job__info--position">{listing.position}</h3>
+            </div>
+            <div className="job__info--detail-container">
+                <p className="job__info--posted-at">{listing.postedAt}</p>
+                &#8226;
+                <p className="job__info--contract">{listing.contract}</p>
+                &#8226;
+                <p className="job__info--location">{listing.location}</p>
+            </div>
         </div>
     );
 }
