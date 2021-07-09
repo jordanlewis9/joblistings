a63fe6d938f8be8cd822f79f4f7615c7779d0ad4
Jordan Lewis <jlewis2008@live.com>
2021-07-08 21:07:35 -0400

    listings card mobile css nearly complete

--- src/components/listings/ListingsCard.js
+++ src/components/listings/ListingsCard.js
@@ -3,8 +3,9 @@ import JobInfo from './JobInfo';
 import JobFilters from './JobFilters';
 
 const ListingsCard = (props) => {
+
     return (
-        <div className="listings__card">
+        <div className={`listings__card ${props.listing.featured ? 'listings__featured' : ''}`}>
             <JobInfo listing={props.listing} />
             <hr className="listings__card--hr"/>
             <JobFilters filtersArray={props.filtersArray} role={props.listing.role} languages={props.listing.languages} level={props.listing.level} tools={props.listing.tools} addFilter={props.addFilter}/>
