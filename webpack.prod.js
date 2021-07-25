2d21287849b2fd160df90b0ff1e6eb98e349e99a
Jordan <jlewis2008@live.com>
2021-07-25 15:19:52 -0400

    configuring webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,37 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
+const common = require("./webpack.common");
 
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "[name].[contenthash].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "images"
+    path: path.resolve(__dirname, "build"),
+    assetModuleFilename: "images/[name][ext]"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   }
 })
